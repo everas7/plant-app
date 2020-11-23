7711f1f1efa161a5e54f987c340268c6592a3520
Enmanuel Veras <enmaveras07@gmail.com>
2020-11-22 23:26:00 -0400

    Moves header to its own component

---
+++ lib/screens/home/components/header_with_search.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'package:plant_app/constants.dart' as Constants;
+
+class HeaderWithSearch extends StatelessWidget {
+  const HeaderWithSearch({
+    Key key,
+    @required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size.height * 0.2,
+      child: Stack(children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(
+            left: Constants.DEFAULT_PADDING,
+            right: Constants.DEFAULT_PADDING,
+            bottom: Constants.DEFAULT_PADDING + 36,
+          ),
+          height: size.height * 0.2 - 27,
+          decoration: BoxDecoration(
+            color: Constants.PRIMARY_COLOR,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(36),
+              bottomRight: Radius.circular(36),
+            ),
+          ),
+          child: Row(
+            children: <Widget>[
+              Text(
+                'Hi Mark!',
+                style: Theme.of(context)
+                    .textTheme
+                    .headline5
+                    .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              Spacer(),
+              Image.asset("assets/images/logo.png"),
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.symmetric(horizontal: Constants.DEFAULT_PADDING),
+            padding:
+                EdgeInsets.symmetric(horizontal: Constants.DEFAULT_PADDING),
+            height: 54,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 10),
+                  blurRadius: 50,
+                  color: Constants.PRIMARY_COLOR.withOpacity(0.23),
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    onChanged: (value) {},
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      hintStyle: TextStyle(
+                        color: Constants.PRIMARY_COLOR.withOpacity(0.5),
+                      ),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
+                  ),
+                ),
+                SvgPicture.asset("assets/icons/search.svg"),
+              ],
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+}
